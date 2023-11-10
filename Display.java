9ef095d44427f7a7f149c346e11014149ea57876
Aysha-shamli <ayshashamli1999@gmail.com>
2023-11-10 20:50:56 +0530

    added Display.java program

---
+++ Display.java
@@ -0,0 +1,12 @@
+package com.EmployeeWage;
+class Employee{
+    static void displayWelcomeMessage() {
+        System.out.println("Welcome to Employee Wage Computation Program!");
+    }
+}
+public class Display {
+    public static void main(String[] args) {
+        Employee.displayWelcomeMessage();
+    }
+
+}
